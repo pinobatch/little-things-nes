a4a7f8278a3b85ccf3269f721a1a85e47911005b
Damian Yerrick <git@pineight.com>
2020-01-18 19:09:04 -0500

    eq sinusweep: store wave position in registers
    I realized that Y was unused and could be reused for wave position.
    This let me cut the main loop of sinusweep down to 66 cycles for a
    sample rate of 27118 Hz.
    
    Also document the sweep's mathematical behavior in comments
    
    - Sweep begins at 1 cycle per 1024 samples or 26.48 Hz
    - Sweep ends at 1 cycle per 2 samples or 13559 Hz
    - Increases by 1/65536 of current value, doubling about every
      45426 sample periods or 1.675 seconds
    - Thus it crosses all 9 octaves in about 15 seconds

--- eq/src/sinusweep.s
+++ eq/src/sinusweep.s
@@ -1,7 +1,7 @@
 ;
 ; Sine sweep generator
 ;
-; Copyright 2017 Damian Yerrick
+; Copyright 2017, 2020 Damian Yerrick
 ; 
 ; This software is provided 'as-is', without any express or implied
 ; warranty.  In no event will the authors be held liable for any damages
@@ -22,37 +22,42 @@
 
 .export sinusweep
 .import sine4011
+
+.code
+.align 64  ; don't cross page boundaries
 .proc sinusweep
-pos1 = $09
-pos2 = $0A
-pos3 = $0B
 freq0 = $0C
 freq1 = $0D
 freq2 = $0E
 freq3 = $0F
 
+  ; Begin at 1 sample per 4 sample periods, or 1 cycle per 1024
+  ; sample periods
   ldx #0
-  stx pos2
+  ldy #0
   stx freq0
   stx freq1
   stx freq3
   lda #$40
   sta freq2
 
-  ; add frequency to position
 more:
+  ; Each sample period is currently 66 cycles.
+  ; Add frequency to position, stored in XXYY
   lda freq1
   asl a
-  lda freq2
-  adc pos2
-  sta pos2
+  tya
+  adc freq2
+  tay
   txa
   adc freq3
   tax
   lda sine4011,x
   sta $4011
 
-  ; increase frequency logarithmically
+  ; increase frequency exponentially
+  ; f = f0 * (65537/65536)^t
+  ; or a doubling about every 45426 samples
   clc
   lda freq2
   adc freq0
@@ -66,10 +71,15 @@ more:
   lda #0
   adc freq3
   sta freq3
+
+  ; Terminate when frequency is 1 cycles per 2 sample periods
+  ; for a total of 9 octaves
   bpl more
   rts
 .endproc
 
+; this triangle wave was used for testing before sine4011
+; was finished
 .if 0
 .rodata
 .align 256
